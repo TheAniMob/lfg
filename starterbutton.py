dd09d7493336b7b47a88f0f67ae62a9d9afe3275
TheAniMob <marcobasile5@icloud.com>
2022-07-10 09:18:59 -0400

    init

---
+++ starterbutton.py
@@ -0,0 +1,6 @@
+from tkinter import *
+
+window = Tk(title='Are you sure you want to run?')
+confirmbutton = Button(window,text='Click Me To Run the Script')
+confirmbutton.pack()
+window.mainloop()
